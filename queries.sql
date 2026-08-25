3967b2d9064bf312deff5fc7ce6b8363695c8487
Yassm1m <yasmim.costa@etec.gov.sp.br>
2026-08-25 02:36:45 -0300

    adiciona queries SQL de análise do banco

--- queries.sql
+++ queries.sql
@@ -1,19 +1,22 @@
--- Queries Hotel API--
+-- Queries Hotel API
 
--- 1. Total de clientes
+-- 1. Quantos clientes temos na base?
 SELECT COUNT(*) AS total_clientes
 FROM customers;
 
--- 2. Total de quartos
+
+-- 2. Quantos quartos temos cadastrados?
 SELECT COUNT(*) AS total_quartos
 FROM rooms;
 
--- 3. Reservas em aberto
+
+-- 3. Quantas reservas em aberto o hotel possui no momento?
 SELECT COUNT(*) AS reservas_em_aberto
 FROM reservations
 WHERE status = 'OPEN';
 
--- 4. Quartos vagos
+
+-- 4. Quantos quartos temos vagos no momento?
 SELECT COUNT(*) AS quartos_vagos
 FROM rooms r
 WHERE r.id NOT IN (
@@ -22,25 +25,31 @@ WHERE r.id NOT IN (
     WHERE status = 'IN_USE'
 );
 
--- 5. Quartos ocupados
+
+-- 5. Quantos quartos temos ocupados no momento?
 SELECT COUNT(DISTINCT room_id) AS quartos_ocupados
 FROM reservations
 WHERE status = 'IN_USE';
 
--- 6. Reservas futuras
+
+-- 6. Quantas reservas futuras o hotel possui?
 SELECT COUNT(*) AS reservas_futuras
 FROM reservations
 WHERE check_in > CURDATE()
   AND status IN ('OPEN', 'IN_USE');
 
--- 7. Quarto mais caro
+
+-- 7. Qual o quarto mais caro do hotel?
 SELECT id, room_number, type, price
 FROM rooms
 ORDER BY price DESC
     LIMIT 1;
 
--- 8. Quarto com mais cancelamentos
-SELECT r.id, r.room_number, COUNT(*) AS total_cancelamentos
+
+-- 8. Qual o quarto com maior histórico de cancelamentos?
+SELECT r.id,
+       r.room_number,
+       COUNT(*) AS total_cancelamentos
 FROM reservations res
          JOIN rooms r ON r.id = res.room_id
 WHERE res.status = 'CANCELLED'
@@ -48,24 +57,32 @@ GROUP BY r.id, r.room_number
 ORDER BY total_cancelamentos DESC
     LIMIT 1;
 
--- 9. Clientes por quarto
-SELECT r.id, r.room_number,
+
+-- 9. Liste todos os quartos e a quantidade de clientes
+SELECT r.id,
+       r.room_number,
        COUNT(DISTINCT res.customer_id) AS total_clientes
 FROM rooms r
          LEFT JOIN reservations res ON res.room_id = r.id
 GROUP BY r.id, r.room_number
 ORDER BY r.id;
 
--- 10. 3 quartos com mais ocupações
-SELECT r.id, r.room_number, COUNT(*) AS total_ocupacoes
+
+-- 10. Quais são os 3 quartos que possuem
+SELECT r.id,
+       r.room_number,
+       COUNT(*) AS total_ocupacoes
 FROM reservations res
          JOIN rooms r ON r.id = res.room_id
 GROUP BY r.id, r.room_number
 ORDER BY total_ocupacoes DESC
     LIMIT 3;
 
--- 11. Clientes com mais reservas
-SELECT c.id, c.name, c.email,
+
+-- 11. Os 10 clientes com maior histórico de reservas
+SELECT c.id,
+       c.name,
+       c.email,
        COUNT(res.id) AS total_reservas
 FROM customers c
          JOIN reservations res ON res.customer_id = c.id
@@ -73,7 +90,8 @@ GROUP BY c.id, c.name, c.email
 ORDER BY total_reservas DESC
     LIMIT 10;
 
--- 12. Média do valor das reservas
+
+-- 12. Receita média gerada,
 SELECT AVG(r.price) AS receita_media
 FROM reservations res
          JOIN rooms r ON r.id = res.room_id
