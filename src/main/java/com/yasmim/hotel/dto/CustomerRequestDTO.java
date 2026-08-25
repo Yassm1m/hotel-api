fcfe0b76031959fecd5e0bf29025c4a3eef35048
Yassm1m <yasmim.costa@etec.gov.sp.br>
2026-08-25 02:07:12 -0300

    Adicionando a  validação nos DTOs de customer

--- src/main/java/com/yasmim/hotel/dto/CustomerRequestDTO.java
+++ src/main/java/com/yasmim/hotel/dto/CustomerRequestDTO.java
@@ -1,5 +1,8 @@
 package com.yasmim.hotel.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.Valid;
 import lombok.*;
 
 @Getter
@@ -8,9 +11,20 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CustomerRequestDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
     private String name;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
     private String email;
+
+    @NotBlank(message = "Telefone é obrigatório")
     private String phoneNumber;
+
+    @NotBlank(message = "CPF é obrigatório")
     private String cpf;
+
+    @Valid
     private AddressDTO address;
 }
